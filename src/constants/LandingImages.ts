47c6ee53271f9715ad4806c57db04290c5e5d8d1
Héctor <wobwez@gmail.com>
2024-10-03 07:31:30 -0400

    refactor: add constant for images import

---
+++ src/constants/LandingImages.ts
@@ -0,0 +1,12 @@
+export { default as MenuExample1Webp } from "../assets/images/example/menu-example-1.webp";
+export { default as MenuExample1Jpg } from "../assets/images/example/menu-example-1.jpg";
+export { default as MenuExample2Webp } from "../assets/images/example/menu-example-2.webp";
+export { default as MenuExample2Jpg } from "../assets/images/example/menu-example-2.jpg";
+
+export { default as Benefits1 } from "../assets/images/benefits/benefits-1.svg";
+export { default as Benefits2 } from "../assets/images/benefits/benefits-2.svg";
+export { default as Benefits3 } from "../assets/images/benefits/benefits-3.svg";
+export { default as Benefits4 } from "../assets/images/benefits/benefits-4.svg";
+
+export { default as Price1 } from "../assets/images/prices/price-1.svg";
+export { default as Price2 } from "../assets/images/prices/price-2.svg";
