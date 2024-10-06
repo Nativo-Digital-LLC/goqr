5acfc63b3e0fd32d56e6c75bec024007f2200874
Héctor <wobwez@gmail.com>
2024-10-06 15:20:51 -0400

    feat: add not found screen styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,27 @@
+import HomeContainer from "./containers/HomeContainer";
+
 export default function NotFoundPage() {
 	return (
-		<div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-			<h1>404</h1>
-		</div>
+		<HomeContainer
+			navBackgroundColor="--tertiary"
+			footerBackgroundColor="--tertiary"
+		>
+			<div className="flex flex-col justify-center items-center flex-1">
+				<div className="bg-[--tertiary] w-full flex-grow flex flex-col mt-[50px] items-center">
+					<h1 className="md:text-[100px] text-[60px] font-[700]">
+						404
+					</h1>
+					<p className="md:text-[22px] text-[18px]">
+						Página no encontrada
+					</p>
+					<a
+						href="/"
+						className="mt-[40px] underline underline-offset-[4px] decoration-[3px] decoration-[--secondary] font-[500]"
+					>
+						Volver al incio
+					</a>
+				</div>
+			</div>
+		</HomeContainer>
 	);
 }
