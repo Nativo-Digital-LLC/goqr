9f589dd3b5cdbd190d5cb8243820eb78bad51a69
Héctor <wobwez@gmail.com>
2024-10-07 07:30:35 -0400

    copy: change benefits text

--- src/pages/home/sections/Benefits.tsx
+++ src/pages/home/sections/Benefits.tsx
@@ -6,7 +6,7 @@ export default function Benefits() {
 		<div className="md:py-[80px] h-auto bg-[--primary] py-[40px] px-[20px] relative z-[3]">
 			<div className="text-center">
 				<h2 className="text-[25px] font-[700] mb-[20px] md:text-[35px] sm:text-[30px]">
-					QrCode menu benefits
+					Beneficios de un Menú QR
 				</h2>
 			</div>
 			<div className="flex flex-col justify-center items-center">
