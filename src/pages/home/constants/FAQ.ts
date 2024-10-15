9fb5f105b49dea626d01124b4a284e8e684f7c52
Luis Santiago <lsantiago@nd.com.do>
2024-10-14 20:30:45 -0400

    copy: elimina respuesta incorrecta

--- src/pages/home/constants/FAQ.ts
+++ src/pages/home/constants/FAQ.ts
@@ -5,41 +5,39 @@ const faq: FAQItemProps[] = [
 		question:
 			"Ya tengo menús hechos en papel, ¿por qué es mejor tu menú digital?",
 		answer: `Nuestro menú digital en código QR es sin contacto y siempre está "en la mesa". Los clientes no tendrán que esperar a que el camarero les acerque el menú, ni tendrán que buscarlo ellos mismos. Cuanto menos se espere y menos acciones hagan falta para hacer un pedido, más pedidos se harán.
-		
+
 		Los menús de papel no se pueden editar, no hay manera de crear promociones o descuentos rápidamente.
-		
+
 		Para mantener la relevancia y una buena presentación del menú de papel, tiene que volver a imprimirse de vez en cuando.`,
 	},
 	{
 		question:
 			"Ya tengo un menú en código QR con un archivo PDF, ¿en qué se diferencia tu menú digital de este?",
 		answer: `El archivo PDF no es interactivo. Tiene todas las desventajas de un menú de papel. Tu menú en código QR se parecerá a un texto con una lista de precios y no parecerá una moderna página que aumentará tus ventas.
-		
+
 			No podrás añadir fotografías ni descripciones a cada artículo del menú PDF, se haría enorme y muy poco legible.
-			
+
 			Cuando escanees un código QR de un menú PDF, tendrás que descargarlo en tu teléfono, puede ser un problema con una velocidad de Internet baja o si tus clientes simplemente no quieren descargar nada en sus teléfonos.
-			
+
 			Los menús PDF tampoco se pueden editar rápidamente. ¿Tienes una tarta de queso nueva? Ahora tendrás que rehacer el PDF entero y volver a subirlo a tu host. En nuestro menú digital, se tarda unos 20 segundos en hacer una operación similar y se puede realizar fácilmente incluso con tu Android o iPhone.`,
 	},
 	{
 		question:
 			"¿Tengo que llamarte o enviarte un mensaje cada vez que quiero cambiar algo en el menú de mi código QR?",
 		answer: `No es necesario. Puedes editar tu menú en código QR siempre que quieras. Hemos diseñado una interfaz de administrador que te facilitará la edición de tu menú, tanto desde un sobremesa como desde dispositivos móviles.
-		
+
 			También puedes dar acceso a tus empleados a tu menú digital, para que puedan mantenerlo actualizado ellos mismos, sin que tengas que realizar tú las acciones.`,
 	},
 	{
 		question:
 			"¿En primer lugar se podría probar de manera gratuita el menú digital?",
 		answer: `Por supuesto. Puedes probar antes nuestro servicio de menú digital, y luego decidir, si le vendrá bien a tu negocio o no. Para probarlo no tendrás que añadir información de tu tarjeta de crédito ni pagar una suscripción. Nuestro menú en código QR es gratuito durante un mes completo.
-		
+
 			Lo único que tienes que hacer es crear una cuenta y te añadiremos automáticamente un mes gratis.`,
 	},
 	{
 		question: "¿Es posible probar primero tu menú digital gratis?",
-		answer: `Los códigos QR antiguos seguirán funcionando si cambias el nombre de tu restaurante, cafetería o bar. O si cambias cualquier otra cosa de tu menú digital.
-		
-			Incluso el enlace, que lleva a tu menú digital, se puede cambiar en cualquier momento, y el código QR antiguo seguirá funcionando.`,
+		answer: `Los códigos QR antiguos seguirán funcionando si cambias el nombre de tu restaurante, cafetería o bar. O si cambias cualquier otra cosa de tu menú digital.`,
 	},
 	{
 		question: "",
