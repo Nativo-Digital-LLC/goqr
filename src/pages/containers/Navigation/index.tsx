4e43d42278b17a66b5a7c4aa5f3eee2070426f40
Héctor <wobwez@gmail.com>
2024-12-01 14:44:26 -0400

    feat: add sticky navigation

--- src/pages/containers/Navigation/index.tsx
+++ src/pages/containers/Navigation/index.tsx
@@ -1,18 +1,42 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./index.css";
 
 export default function Navigation({ url }: { url: string }) {
 	const [isOpen, setIsOpen] = useState(false);
+	const [y, setY] = useState(window.scrollY);
+	const [isSticky, setIsSticky] = useState(false);
 
 	const selectedClass = "border-b-[2px] border-b-[var(--text)] font-[800]";
+
+	const handleNavigation = useCallback(
+		(e: Event) => {
+			const window = e.currentTarget as Window;
+			if (y > window.scrollY) {
+				setIsSticky(true);
+			} else if (y < window.scrollY) {
+				setIsSticky(false);
+			}
+			setY(window.scrollY);
+		},
+		[y]
+	);
+
+	useEffect(() => {
+		setY(window.scrollY);
+		window.addEventListener("scroll", handleNavigation);
+
+		return () => {
+			window.removeEventListener("scroll", handleNavigation);
+		};
+	}, [handleNavigation]);
 
 	return (
 		<div
 			className={`navigation-container ${
 				isOpen && "navigation-container-active"
-			}`}
+			} ${isSticky && "navigation-container-sticky"}`}
 		>
 			<ul>
 				<li className={`${url == "home" && selectedClass}`}>
